09217c37bca559154f819268a5ac683cf3285302
drnebbie <benhacundela@gmail.com>
2024-06-17 14:56:57 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    "rules": {
+          "react/no-unescaped-entities": ["error", {
+            "forbid": ["'", "&apos;", "&#39;"]
+          }]
+        }
+      
+};
 
 export default nextConfig;
